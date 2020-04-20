ed43600b6898294f7cdfe7314adbf33075d220f7
Sercan-dev <sercancap@gmail.com>
2020-04-20 20:45:54 +0200

    another change

--- src/components/contact.js
+++ src/components/contact.js
@@ -6,15 +6,36 @@ const FormPage = () => {
     <MDBContainer>
     <MDBRow className="d-flex justify-content-center">
         <MDBCol md="8">
-        <form className="form">
+        <form className="form" method="POST" dat-netlify="true">
             <p className="h5 text-center mb-4">Write me</p>
             <div className="grey-text">
-            <MDBInput label="Your name" icon="user" group type="text" validate error="wrong"
+            <MDBInput 
+                label="Your name" 
+                name="name"
+                icon="user" 
+                group type="text" 
+                validate error="wrong"
                 success="right" />
-            <MDBInput label="Your email" icon="envelope" group type="email" validate error="wrong"
+            <MDBInput 
+                label="Your email"
+                name="email"
+                icon="envelope" 
+                group type="email" 
+                validate error="wrong"
                 success="right" />
-            <MDBInput label="Subject" icon="tag" group type="text" validate error="wrong" success="right" />
-            <MDBInput type="textarea" rows="2" label="Your message" icon="pencil-alt" />
+            <MDBInput 
+                label="Subject" 
+                name="subject"
+                icon="tag" 
+                group type="text" 
+                validate error="wrong" 
+                success="right" />
+            <MDBInput 
+                type="textarea" 
+                rows="2" 
+                label="Your message" 
+                name=" message"
+                icon="pencil-alt" />
             </div>
             <div className="text-center">
             <MDBBtn outline color="success">
